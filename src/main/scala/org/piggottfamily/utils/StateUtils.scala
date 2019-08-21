a00068cbb8ca258c329d5386f652ce24c92bade0
Alex-At-Home <alex.piggott@elastic.co>
2019-08-21 17:04:25 -0400

    Interim check in while i remove all the code

--- src/main/scala/org/piggottfamily/utils/StateUtils.scala
+++ src/main/scala/org/piggottfamily/utils/StateUtils.scala
@@ -10,12 +10,16 @@ object StateUtils {
     case object NestedStashException extends Exception("Cannot stash during an unstash operation")
 
     trait StateContext[S, A, B] {
-      /** The current set of events is from an unstash (can't stash if so) */
-      def in_unstash: Boolean
+      /** Whether we are the isReprocessed element at the end of an unstash */
+      def isReprocessed: Boolean = unstashing && !hasStash
+      /** Whether there is anything in the stash (convenience to avoid "reverseStash.nonEmpty") */
+      def hasStash: Boolean = reverseStash.nonEmpty
+      /** The current set of elements is from an unstash (can't stash if so, unless also "reprocessed") */
+      def unstashing: Boolean
       /** The current stash */
-      def reverse_stash: List[A]
-      /** The current list of output events */
-      def reverse_out: List[B]
+      def reverseStash: List[A]
+      /** The current list of output elements */
+      def reverseOut: List[B]
 
       // Transition handlers:
       def noChange: StateTransition[S, A, B] = NoChange()
@@ -28,8 +32,12 @@ object StateUtils {
       def stashElement(new_s: S, a: A): StateTransition[S, A, B] = StashChange(new_s, a)
       def stashElements(as: List[A]): StateTransition[S, A, B] = StashElements(as)
       def stashElements(new_s: S, as: List[A]): StateTransition[S, A, B] = StashChangeMulti(new_s, as)
-      def unstashAll(reprocess_event: Boolean = false, remap: List[A] => List[A] = _.reverse): StateTransition[S, A, B] = UnstashAll(reprocess_event, remap)
-      def discardStash(): StateTransition[S, A, B] = unstashAll(remap = _ => Nil)
+      def unstashAll(reprocess_element: Boolean): StateTransition[S, A, B] = UnstashAll(reprocess_element)
+      def unstashAll(reprocess_element: Boolean, remap: List[A] => List[A]): StateTransition[S, A, B] = UnstashAll(reprocess_element, remap)
+      def unstashAll(new_s: S, reprocess_element: Boolean): StateTransition[S, A, B] = UnstashAllChange(new_s, reprocess_element)
+      def unstashAll(new_s: S, reprocess_element: Boolean, remap: List[A] => List[A]): StateTransition[S, A, B] = UnstashAllChange(new_s, reprocess_element, remap)
+      def discardStash(): StateTransition[S, A, B] = unstashAll(reprocess_element = false, remap = _ => Nil)
+      def discardStash(new_s: S): StateTransition[S, A, B] = unstashAll(new_s, reprocess_element = false, remap = _ => Nil)
     }
 
     // In
@@ -37,6 +45,9 @@ object StateUtils {
     object StateEvent {
       case class Next[S, A ,B](ctx: StateContext[S, A, B], s: S, a: A) extends StateEvent[S, A, B]
       case class Complete[S, A, B](ctx: StateContext[S, A, B], s: S) extends StateEvent[S, A, B]
+
+      case class StashStart[S, A, B](s: S, unstash_element: StateEvent[S, A, B], ordered_stash: List[A]) extends StateEvent[S, A, B]
+      case class StashEnd[S, A, B](s: S, unstash_element: StateEvent[S, A, B], ordered_stash: List[A]) extends StateEvent[S, A, B]
     }
 
     sealed trait StateEvent2[S, A, B]
@@ -46,10 +57,32 @@ object StateUtils {
       case class Next[S, A, B](ctx: StateContext[S, A, B], s: S, prev_a: A, a: A) extends StateEvent2[S, A, B]
       /** Note prev_a might have been stashed, ie not processed */
       case class Complete[S, A, B](ctx: StateContext[S, A, B], s: S, prev_a: Option[A]) extends StateEvent2[S, A, B]
+
+      case class StashStart[S, A, B](s: S, a: Option[A], unstash_element: StateEvent2[S, A, B], ordered_stash: List[A]) extends StateEvent2[S, A, B]
+      case class StashEnd[S, A, B](s: S, prev_a: Option[A], unstash_element: StateEvent2[S, A, B], ordered_stash: List[A]) extends StateEvent2[S, A, B]
+
+      /** Shortcut for pulling out the element that will become prev next iteration */
+      object EventsThatGeneratePrevs {
+        def unapply[S, A, B](ev: StateEvent2[S, A, B]): Option[A] = ev match {
+          case First(_, a) => Some(a)
+          case Next(_, _, a) => Some(a)
+          case _ => None
+        }
+      }
     }
 
     // Out
     sealed trait StateTransition[S, A, B]
+    object StateTransition {
+      /** Utility for spotting stash related commands (that can't be issued inside stashes) */
+      object IsUnstashRelated {
+        def unapply(t: StateTransition[_, _, _]): Option[StateTransition[_, _, _]] = t match {
+          case _: StashElement[_, _, _] | _: StashElements[_, _, _] |
+               _: StashChange[_, _, _] | _: StashChangeMulti[_, _, _] | _: UnstashAll[_, _, _] => Some(t)
+          case  _ => None
+        }
+      }
+    }
     case class NoChange[S, A, B]() extends StateTransition[S, A, B]
     case class StateChange[S, A, B](s: S) extends StateTransition[S, A, B]
     case class ConstantState[S, A, B](b: B) extends StateTransition[S, A, B]
@@ -64,18 +97,19 @@ object StateUtils {
     case class StashChange[S, A, B](s: S, a: A) extends StateTransition[S, A, B]
     /** Note bs should be in "reverse order" wrt the input, eg last :: last_but_one :: ... :: first :: Nil */
     case class StashChangeMulti[S, A, B](s: S, as: List[A]) extends StateTransition[S, A, B]
-    case class UnstashAll[S, A, B](reprocess_event: Boolean = false, remap: List[A] => List[A] = UnstashAll.defaultOrdering[A](_)) extends StateTransition[S, A, B]
+    case class UnstashAll[S, A, B](reprocess_element: Boolean = false, remap: List[A] => List[A] = UnstashAll.defaultOrdering[A](_)) extends StateTransition[S, A, B]
     object UnstashAll {
       def defaultOrdering[A](in: List[A]): List[A] = in.reverse
       def discard[S, A, B]: UnstashAll[S, A, B] = UnstashAll(remap = _ => Nil)
     }
+    case class UnstashAllChange[S, A, B](new_s: S, reprocess_element: Boolean = false, remap: List[A] => List[A] = UnstashAll.defaultOrdering[A](_)) extends StateTransition[S, A, B]
 
     // Final
     class FoldStateComplete[S, B](val s: S, val reverse_bs: List[B]) {
       /** The "correctly" ordered version of the list. Recalculated every time */
       def bs: List[B] = reverse_bs.reverse
 
-      override def toString: String = s"FoldStateComplete($s, $reverse_bs)"
+      override def toString: String = s"FoldStateComplete.Raw($s, $reverse_bs)"
     }
     object FoldStateComplete {
       def unapply[S, B](fsc: FoldStateComplete[S, B]): Option[(S, List[B])] = Some(fsc.s, fsc.bs)
@@ -94,24 +128,22 @@ object StateUtils {
     * TODO scaladoc
     */
   def foldLeft[S, A, B]
-  (l: Seq[A], init: S, typeHint: Class[B]) //TODO Seq?
+    (l: Seq[A], init: S, typeHint: Class[B])
     (transition: StateEvent[S, A, B] => StateTransition[S, A, B])
   : FoldStateComplete[S, B] =
   {
     val init_temp_state = TempState[S, A, B](init)
     val phase1 = l.foldLeft(init_temp_state) { (acc, v) =>
-      handle_unstash_command(
-        Some(v), transition,
-        handle_result(
-          acc, transition(StateEvent.Next(acc, acc.s, v))
-        )
+      command_completion(
+        Some(v),
+        transition, StateEvent.Next(acc, acc.s, v),
+        handle_result(acc, _)
       )
     }
-    val phase2 = handle_unstash_command(
-      None, transition,
-      handle_result(
-        phase1, transition(StateEvent.Complete(phase1, phase1.s))
-      )
+    val phase2 = command_completion[S, A, B](
+      None,
+      transition, StateEvent.Complete(phase1, phase1.s),
+      handle_result(phase1, _)
     )
     new FoldStateComplete(phase2.s, phase2.bs)
   }
@@ -121,27 +153,12 @@ object StateUtils {
     * TODO scaladoc
     */
   def foldLeft[S, A]
-  (l: Seq[A], init: S) //TODO Seq?
+  (l: Seq[A], init: S)
     (transition: StateEvent[S, A, A] => StateTransition[S, A, A])
+    (implicit ctag: reflect.ClassTag[A])
   : FoldStateComplete[S, A] =
-  {
-    val init_temp_state = TempState[S, A, A](init)
-    val phase1 = l.foldLeft(init_temp_state) { (acc, v) =>
-      handle_unstash_command(
-        Some(v), transition,
-        handle_result(
-          acc, transition(StateEvent.Next(acc, acc.s, v))
-        )
-      )
-    }
-    val phase2 = handle_unstash_command(
-      None, transition,
-      handle_result(
-        phase1, transition(StateEvent.Complete(phase1, phase1.s))
-      )
-    )
-    new FoldStateComplete(phase2.s, phase2.bs)
-  }
+    foldLeft(l, init, ctag.runtimeClass.asInstanceOf[Class[A]])(transition)
+
   /** A version of foldLeft for use in state transition like activities
     * where the state transition is a function of the current and previous elements
     * Note that you incur the cost of a reverse on the output list
@@ -149,26 +166,32 @@ object StateUtils {
     * TODO seq?
     */
   def foldLeft2[S, A, B]
-  (l: Seq[A], init: S, typeHint: Class[B])
+    (l: Seq[A], init: S, typeHint: Class[B])
     (transition: StateEvent2[S, A, B] => StateTransition[S, A, B])
   : FoldStateComplete[S, B] =
   {
+    // (NOTE: I tried rewriting this in terms of foldLeft but stash logic made this
+    // more trouble than it was worth, though getting rid of "command_completion2"
+    // would have been nice)
     val init_temp_state = TempState[S, A, B](init)
     val phase1 = l.foldLeft(init_temp_state) {
       case (acc @ TempState(_, _, None, _, _), v) =>
-        handle_unstash_command2_and_prev(
-          None, Some(v), transition,
-          handle_result(acc, transition(StateEvent2.First(acc, acc.s, v)))
+        command_completion2(
+          None, Some(v),
+          transition, StateEvent2.First(acc, acc.s, v),
+          handle_result(acc, _)
         )
       case (acc @ TempState(_, _, Some(prev_v), _, _), v) =>
-        handle_unstash_command2_and_prev(
-          Some(prev_v), Some(v), transition,
-          handle_result(acc, transition(StateEvent2.Next(acc, acc.s, prev_v, v)))
+        command_completion2(
+          Some(prev_v), Some(v),
+          transition, StateEvent2.Next(acc, acc.s, prev_v, v),
+          handle_result(acc, _)
         )
     }
-    val phase2 = handle_unstash_command2_and_prev(
-      phase1.prev, None,  transition,
-      handle_result(phase1, transition(StateEvent2.Complete(phase1, phase1.s, phase1.prev)))
+    val phase2 = command_completion2[S, A, B](
+      phase1.prev, None,
+      transition, StateEvent2.Complete(phase1, phase1.s, phase1.prev),
+      handle_result(phase1, _)
     )
     new FoldStateComplete(phase2.s, phase2.bs)
   }
@@ -180,29 +203,11 @@ object StateUtils {
     * TODO seq?
     */
   def foldLeft2[S, A]
-  (l: Seq[A], init: S)
+    (l: Seq[A], init: S)
     (transition: StateEvent2[S, A, A] => StateTransition[S, A, A])
+    (implicit ctag: reflect.ClassTag[A])
   : FoldStateComplete[S, A] =
-  {
-    val init_temp_state = TempState[S, A, A](init)
-    val phase1 = l.foldLeft(init_temp_state) {
-      case (acc @ TempState(_, _, None, _, _), v) =>
-        handle_unstash_command2_and_prev(
-          None, Some(v), transition,
-          handle_result(acc, transition(StateEvent2.First(acc, acc.s, v)))
-        )
-      case (acc @ TempState(_, _, Some(prev_v), _, _), v) =>
-        handle_unstash_command2_and_prev(
-          Some(prev_v), Some(v), transition,
-          handle_result(acc, transition(StateEvent2.Next(acc, acc.s, prev_v, v)))
-        )
-    }
-    val phase2 = handle_unstash_command2_and_prev(
-      phase1.prev, None,  transition,
-      handle_result(phase1, transition(StateEvent2.Complete(phase1, phase1.s, phase1.prev)))
-    )
-    new FoldStateComplete(phase2.s, phase2.bs)
-  }
+    foldLeft2(l, init, ctag.runtimeClass.asInstanceOf[Class[A]])(transition)
 
   // Internals
 
@@ -212,14 +217,14 @@ object StateUtils {
     prev: Option[A] = None,
     stash: List[A] = Nil, unstash: Option[UnstashAll[S, A, B]] = None
   ) extends StateContext[S, A, B] {
-    def reverse_stash: List[A] = stash
-    def in_unstash: Boolean = unstash.nonEmpty
-    def reverse_out: List[B] = bs
+    def reverseStash: List[A] = stash
+    def unstashing: Boolean = unstash.nonEmpty
+    def reverseOut: List[B] = bs
   }
 
   /** Translates between the StateUtils.foldLeft and the actual foldLeft responses */
   private def handle_result[S, A, B]
-  (acc: TempState[S, A, B], res: StateTransition[S, A, B]): TempState[S, A, B] = res match
+    (acc: TempState[S, A, B], res: StateTransition[S, A, B]): TempState[S, A, B] = res match
   {
     case NoChange() => acc
     case StateChange(new_s) => acc.copy(s = new_s)
@@ -233,28 +238,37 @@ object StateUtils {
     case StashChangeMulti(new_s, as) => acc.copy(s = new_s, stash = as ++ acc.stash)
 
     case unstash_command: UnstashAll[S, A, B] => acc.copy(unstash = Some(unstash_command))
+    case us_cmd: UnstashAllChange[S, A, B] =>
+      acc.copy(s = us_cmd.new_s, unstash = Some(UnstashAll(us_cmd.reprocess_element, us_cmd.remap)))
   }
 
   /** Additional logic for unstashing */
-  private def handle_unstash_command[S, A, B]
-  (
+  private def command_completion[S, A, B](
     maybe_v: Option[A],
     transition: StateEvent[S, A, B] => StateTransition[S, A, B],
-    new_acc: TempState[S, A, B]
-  ): TempState[S, A, B] = new_acc match
+    result: StateEvent[S, A, B],
+    result_handler: StateTransition[S, A, B] => TempState[S, A, B]
+  ): TempState[S, A, B] = (transition andThen result_handler)(result) match
   {
-    case TempState(_, _, _, stash, Some(UnstashAll(reprocess_event, remap))) =>
-      val list = remap(stash)
-      val interm = list.foldLeft(new_acc) { (acc, v) => //(only one level of stashing allowed)
-        val transition_result = transition(StateEvent.Next(acc, acc.s, v)) match {
-          case _: StashElement[_, _, _] | _: StashElements[_, _, _] |
-               _: StashChange[_, _, _] | _: StashChangeMulti[_, _, _] | _: UnstashAll[_, _, _] =>
-            throw NestedStashException
+    case new_s @ TempState(_, _, _, stash, Some(UnstashAll(reprocess_element, remap))) =>
+      val element_list = remap(stash)
+      val event_fn_list: List[TempState[S, A, B] => StateEvent[S, A, B]] = (
+        ((acc: TempState[S, A, B]) => StateEvent.StashStart(acc.s, result, element_list))
+        ::
+        element_list.map(v => ((acc: TempState[S, A, B]) => StateEvent.Next(acc, acc.s, v)))
+      ) ++ List(
+        (acc: TempState[S, A, B]) => StateEvent.StashEnd(acc.s, result, element_list)
+      )
+
+      val interm = (event_fn_list.foldLeft(new_s) { (acc, ev_fn) => //(only one level of stashing allowed)
+        val transition_result = transition(ev_fn(acc)) match {
+          case StateTransition.IsUnstashRelated(_) => throw NestedStashException
           case res => res
         }
         handle_result(acc, transition_result)
-      }
-      val complete = if (reprocess_event) {
+      }).copy(stash = Nil)
+
+      val complete = if (reprocess_element) {
         handle_result(interm, transition(
           maybe_v.map(v => StateEvent.Next(interm, interm.s, v))
             .getOrElse(StateEvent.Complete(interm, interm.s))
@@ -265,18 +279,35 @@ object StateUtils {
     case new_s => new_s
   }
 
-  /** Additional logic for unstashing */
-  private def handle_unstash_command2_and_prev[S, A, B]
-  (
+  /** Additional logic for unstashing
+   *  Basically there's 2 scenarios: (the full sequence is like ...,T, a, b, c, Xp, X, Nn,....)
+   *  (Xp, X)[!reproc], S:(a, b, c) [Xn], gonna do "NE(X, a),  NE(a, b), NE(b, c), NE(c, Xn)"
+   *  (Xp, X)[reproc],  S:(a, b, c) [Xn], gonna do "NE(Xp, a), NE(a, b), NE(b, c), NE(c, X)"
+   *  TODO: the problem is I can't output element and unstash simul which is the second case needs
+   * (where is gets complicated is if I'm acting on Xp not X and then eg t(Xp) > t(a), my processing actually
+   *  needs to be NE(Xpp, a), ..., NE(c, Xp)
+   * ^ahh but ..., a, b, c, Xpp, Xp[UNSTASH], X isn't really how it stashing is _supposed_ to work?
+   *  The alternative is to do
+   *   rp: SS(Xp, X, a), NE(a, b), NE(b, c), SE(c, ??), NE(??, Y)
+   *  !rp: SS(Xp, X, a), NE(a, b), NE(b, c), SE(c, ??), NE(??, ??)
+   * There's really 3 ways you can use foldLeft2: based on processing Xp, based on processing X,
+   * or treating (X, Xp) as a combined event, though really you could use foldLeft in that case?
+   * (and then you might choose to unstash based on looking forward, or looking backwards
+   * ie based on that you could want
+   * (processing Xp looking forward: "...,a,b,c,Xp,X")
+   * (processing Xp looking backward: "Xp,a,b,c,X,...")
+   * if processing X this whole thing makes no sense unless Xp==c right?
+   */
+  private def command_completion2[S, A, B](
     maybe_prev_v: Option[A], maybe_v: Option[A],
     transition: StateEvent2[S, A, B] => StateTransition[S, A, B],
-    new_acc: TempState[S, A, B]
-  ) : TempState[S, A, B] = new_acc match
+    result: StateEvent2[S, A, B],
+    result_handler: StateTransition[S, A, B] => TempState[S, A, B]
+  ) : TempState[S, A, B] = (transition andThen result_handler)(result) match
   {
-    case new_s @ TempState(s, _, _, stash, Some(UnstashAll(reprocess_event, remap))) =>
-      /** Builds the next event based on the overall context */
-      def next_event
-      (
+    case new_s @ TempState(s, _, _, stash, Some(UnstashAll(reprocess_element, remap))) =>
+      /** Builds the next element based on the overall context */
+      def next_element(
         curr_acc: TempState[S, A, B],
         maybe_prev_v: Option[A], maybe_v: Option[A]
       ): StateEvent2[S, A, B] = (maybe_prev_v, maybe_v) match
@@ -286,21 +317,36 @@ object StateUtils {
         case (Some(prev_v), None) => StateEvent2.Complete(curr_acc, curr_acc.s, Some(prev_v))
         case (None, None) => StateEvent2.Complete(curr_acc, curr_acc.s, None)
       }
+      //TODO: not sure this is still valid
+      //eg say I stash (Xp, X), (a, b, c) [Y], gonna do "SS(a...), NE(X, a), NE(a, b), NE(b, c), SE(c), NE(c, Y)"
+      //(Xp, X) [reprocess] -> NE(Xp,a) | (Xp,X) [!reprocess] -> NE(a, b)
 
-      val list = remap(stash)
-      val adjusted_new_acc = new_acc.copy(prev = if (reprocess_event) maybe_prev_v else maybe_v)
-      val interm = list.foldLeft(adjusted_new_acc) { (acc, v) => //(only one level of stashing allowed)
-        val transition_result =  transition(next_event(acc, acc.prev, Some(v))) match {
-          case _: StashElement[_, _, _] | _: StashElements[_, _, _] |
-               _: StashChange[_, _, _] | _: StashChangeMulti[_, _, _] | _: UnstashAll[_, _, _] =>
-            throw NestedStashException
+      val element_list = remap(stash)
+      val event_fn_list: List[TempState[S, A, B] => StateEvent2[S, A, B]] = (
+        ((acc: TempState[S, A, B]) => StateEvent2.StashStart(acc.s, element_list.headOption, result, element_list))
+        ::
+        element_list.map(v => ((acc: TempState[S, A, B]) => next_element(acc, acc.prev, Some(v))))
+      ) ++ List(
+        (acc: TempState[S, A, B]) => StateEvent2.StashEnd(acc.s, element_list.lastOption, result, element_list)
+      )
+
+      //TODO: this and the TODO below are the bits to fix
+      val adjusted_new_s = new_s.copy(prev = if (reprocess_element) maybe_prev_v else maybe_v)
+      val interm = (event_fn_list.foldLeft(adjusted_new_s) { (acc, ev_fn) => //(only one level of stashing allowed)
+        val event = ev_fn(acc)
+        val transition_result = transition(event) match {
+          case StateTransition.IsUnstashRelated(_) => throw NestedStashException
           case res => res
         }
-        handle_result(acc, transition_result)
-      }
-      val complete = if (reprocess_event) {
+        handle_result(acc, transition_result).copy(prev = event match {
+          case StateEvent2.EventsThatGeneratePrevs(a) => Some(a)
+          case _ => None
+        })
+      }).copy(stash = Nil)
+
+      val complete = if (reprocess_element) {
         handle_result(interm, transition(
-          next_event(interm, interm.prev, maybe_v)
+          next_element(interm, interm.prev, maybe_v) //TODO: should be command_completion2??
         ))
       } else interm
       complete.copy(prev = maybe_v, unstash = None)
--- src/test/scala/org/piggottfamily/utils/StateUtilsTests.scala
+++ src/test/scala/org/piggottfamily/utils/StateUtilsTests.scala
@@ -12,7 +12,7 @@ object StateUtilsTests extends TestSuite {
       val input = List(1, 2, 3, 4, 5)
 
       "FoldStateComplete" - {
-        (new FoldStateComplete(1, List(2, 3))).toString ==> "FoldStateComplete(1, List(2, 3))"
+        (new FoldStateComplete(1, List(2, 3))).toString ==> "FoldStateComplete.Raw(1, List(2, 3))"
       }
 
       "foldLeft(l, s)" - {
@@ -73,7 +73,63 @@ object StateUtilsTests extends TestSuite {
           case FoldStateComplete(State(List("complete")), Nil) =>
         }
       }
-      //TODO foldLeft stash
+      "foldLeft(l, s) - stash" - {
+        // Discard all
+        TestUtils.inside(StateUtils.foldLeft(input, 0, classOf[String]) {
+          case StateEvent.Next(ctx, _, _) =>
+            ctx.stashElement(0)
+          case StateEvent.Complete(ctx, _) =>
+            ctx.discardStash()
+        }) {
+          case FoldStateComplete(0, Nil) =>
+        }
+        // Stash and then ignore
+        TestUtils.inside(StateUtils.foldLeft(input, 0, classOf[String]) {
+          case StateEvent.Next(ctx, _, _) =>
+            ctx.stashElement(0)
+          case StateEvent.Complete(ctx, _) =>
+            ctx.stateChange(100)
+        }) {
+          case FoldStateComplete(100, Nil) =>
+        }
+
+        //Simple stash case - exhaustive check of all ctx commands together with "foldLeft2(l, s) - stash"
+
+        case class State(l: List[String])
+
+        val res = StateUtils.foldLeft(input, State(Nil), classOf[String]) {
+          case StateEvent.Next(ctx, _, event) if 1 == event =>
+            ctx.noChange
+
+          case StateEvent.Next(ctx, state, event) if event <= 2 && ctx.unstashing =>
+            ctx.constantState(event.toString)
+          case StateEvent.Next(ctx, state, event) if 2 == event =>
+            ctx.stashElements(List(2, -2))
+
+          case StateEvent.Next(ctx, state, event) if 3 == event && ctx.has_stash =>
+            ctx.unstashAll(reprocess_event = false, remap = (as: List[Int]) => as)
+          case StateEvent.Next(ctx, state, event) if 3 == event => //(never reached)
+            ctx.constantState(List("3b", "3a"))
+
+          case StateEvent.Next(ctx, state, event) if 4 == event && ctx.unstashing =>
+            ctx.constantState(event.toString)
+          case StateEvent.Next(ctx, state, event) if 4 == event =>
+            ctx.stashElement(state.copy(l = event.toString :: state.l), 4)
+
+          case StateEvent.Next(ctx, state, event) if 5 == event =>
+            ctx.stateChange(state.copy(l = event.toString :: state.l), List("5b", "5a"))
+
+          case StateEvent.Complete(ctx, state) if ctx.has_stash =>
+            ctx.unstashAll(state.copy(l = "complete_unstash" :: state.l), reprocess_event = true)
+          case StateEvent.Complete(ctx, _) =>
+            ctx.constantState("final")
+        }
+        TestUtils.inside(res) {
+          case FoldStateComplete(
+            State(List("complete_unstash", "5", "4")), List("2", "-2", "5a", "5b", "4", "final")
+          ) =>
+        }
+      }
 
       "foldLeft2(l, s)" - {
         case class State(sum: Int)
@@ -105,7 +161,70 @@ object StateUtilsTests extends TestSuite {
           case FoldStateComplete("complete_None", List("complete_None")) =>
         }
       }
-      //TODO foldLeft2 stash
+
+      "foldLeft2(l, s) - stash" - {
+        case class State(sum: Int)
+        val res = StateUtils.foldLeft2(input, State(0)) {
+          case StateEvent2.First(ctx, s, event) => //(1)
+            ctx.stateChange(State(s.sum + 100), event)
+
+          case StateEvent2.Next(ctx, s, prev_event, event) if ctx.unstashing => //(5,2), (2,3) = 12
+            ctx.stateChange(State(s.sum + prev_event + event), List(event, prev_event))
+
+          case StateEvent2.Next(ctx, s, prev_event, event) if event == 2 =>
+            ctx.stashElement(State(s.sum + 10000), event)
+          case StateEvent2.Next(ctx, s, prev_event, event) if event <= 3 =>
+            ctx.stashElement(event)
+
+          case StateEvent2.Next(ctx, s, prev_event, event) => //(3,4), (4,5) == 16
+            ctx.stateChange(State(s.sum + prev_event + event), List(event, prev_event))
+
+          case StateEvent2.Complete(ctx, s, prev_event) if ctx.has_stash =>
+            ctx.unstashAll(reprocess_event = true)
+          case StateEvent2.Complete(ctx, s, prev_event) if ctx.reprocessed => //(prev_element is 3 here because of the unstash)
+            ctx.stateChange(s.copy(sum = s.sum + 1000), prev_event.getOrElse(-1))
+          case StateEvent2.Complete(ctx, s, prev_event) => //shouldn't be here
+            ctx.stateChange(State(-100)) //(ie fail test with clear signature)
+        }
+        TestUtils.inside(res) {
+          case FoldStateComplete(
+            State(11128), List(1, 3, 4, 4, 5, 5, 2, 2, 3, 3)
+          ) =>
+        }
+
+        // Want to make sure I get the right next/prev more explicity
+        case class StashState(l: List[String] = Nil)
+        List(true, false).foreach { reproc =>
+          val res2 = StateUtils.foldLeft2(input.take(4).map(_*10), StashState()) {
+            case StateEvent2.First(ctx, s, event) => //(1)
+              ctx.stateChange(s.copy(s"SE2F.$event" :: s.l))
+            case StateEvent2.Next(ctx, s, prev_event, event) if event == 20 && !ctx.unstashing =>
+              ctx.stashElements(List(15, 10))
+            case StateEvent2.Next(ctx, s, prev_event, event) if event == 30 && !ctx.unstashing =>
+              ctx.stashElement(20)
+            case StateEvent2.Next(ctx, s, prev_event, event) if event == 40 && !ctx.reprocessed =>
+              ctx.unstashAll(s.copy(s"SE2N.$prev_event.$event" :: s.l), reprocess_event = reproc)
+            case StateEvent2.Next(ctx, s, prev_event, event) =>
+              ctx.stateChange(s.copy(s"SE2N.$prev_event.$event" :: s.l))
+            case StateEvent2.Complete(ctx, s, prev_event) => //(1)
+              ctx.stateChange(s.copy((s"SE2C.$prev_event" :: s.l).reverse))
+          }
+          TestUtils.inside(res2) {
+            case FoldStateComplete.State(
+              StashState(
+                "SE2F.10" :: "SE2N.30.40" :: "SE2N.10.15" :: "SE2N.15.20" ::  "SE2N.20.30" :: "SE2N.30.40" :: "SE2C.40" :: Nil
+              )
+            ) if reproc =>
+              //Currently this is: StashState(List(SE2F.10, SE2N.30.40, SE2N.30.10, SE2N.10.15, SE2N.15.20, SE2N.20.40, SE2C.Some(40)))
+
+            case FoldStateComplete.State(
+              StashState(
+                "SE2F.10" :: "SE2N.10.15" :: "SE2N.15.20" ::  "SE2N.20.30" :: "SE2C.30" :: Nil
+              )
+            ) if !reproc =>
+          }
+        }
+      }
     }
   }
 }

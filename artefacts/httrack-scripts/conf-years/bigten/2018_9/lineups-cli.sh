2316e06c2ebdd6a843d0c4a8f75d08282357ab11
Alex-At-Home <alex.piggottfamily.co.uk>
2019-10-30 18:00:13 -0400

    Add men and women big ten conferences

---
+++ artefacts/httrack-scripts/conf-years/bigten/2018_9/lineups-cli.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#(source .lineup.env first to set up these variables)
+#CRAWL_PATH=TODO
+#ROOT_URL=TODO
+#(to get the team navigate to https://$ROOT_URL/reports/attendance?id=17900
+# pick the team, select the year, then the team id is the last bit of the URL)
+YEAR=2018
+CONF=bigten
+array=(
+    '450612::Maryland'
+    '450579::Indiana'
+    '450640::Nebraska'
+    '450785::Wisconsin'
+    '450669::Ohio%20St.'
+    '450620::Michigan%20St.'
+    '450583::Iowa'
+    '450621::Michigan'
+    '450688::Purdue'
+    '450576::Illinois'
+    '450679::Penn%20St.'
+    '450666::Northwestern'
+    '450696::Rutgers'
+    '450623::Minnesota'
+)
+
+#TODO add TEAM filter
+
+for index in "${array[@]}" ; do
+    TEAMID="${index%%::*}"
+    TEAM_NAME="${index##*::}"
+    CONF_CRAWL_PATH=$CRAWL_PATH/$CONF/$YEAR/${TEAM_NAME}_${TEAMID}
+
+    if [ "$TEAMID_FILTER" != "" ]; then
+      if [ "$TEAMID_FILTER" != "$TEAMID" ]; then
+        echo "Skipping filtered $TEAM_NAME, [$TEAMID_FILTER] vs [$TEAMID]"
+        continue
+      fi
+    fi
+
+    echo "$TEAMID - $TEAM_NAME"
+    #TODO: only do this if you want to remove and recalc everything, otherwise will find deltas
+    #rm -rf $CONF_CRAWL_PATH
+    mkdir -p $CONF_CRAWL_PATH
+    httrack "http://$ROOT_URL/teams/$TEAMID" --depth=3 --path $CONF_CRAWL_PATH --robots=0 "-*" "+$ROOT_URL/contests/*/box_score" "+$ROOT_URL/game/index/*" +"$ROOT_URL/game/box_score/*?period_no=1" +"$ROOT_URL/game/play_by_play/*"
+
+    #Check for any errors:
+    ERRS=$(grep -c 'Error:' $CONF_CRAWL_PATH/hts-log.txt)
+    if [ $ERRS -gt 0 ]; then
+      echo "************ ERRORS $CONF_CRAWL_PATH/hts-log.txt"
+      exit -1
+    fi
+done

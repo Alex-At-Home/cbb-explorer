b7f5047095e8b8f1448e0ded633ab0c7aef24115
Alex-At-Home <alex.piggottfamily.co.uk>
2019-12-26 14:56:33 -0500

    Add women's American conference to dataset

--- artefacts/scripts/bulk_lineup_import.sh
+++ artefacts/scripts/bulk_lineup_import.sh
@@ -18,7 +18,7 @@ mv $PBP_OUT_DIR/*.ndjson $PBP_OUT_DIR/archive
 rm -f $PBP_OUT_DIR/bulk_import_logs_${CURR_TIME}.log
 for i in $CONFS; do
   echo "******* Extracting conference [$i]"
-#  $PBP_SRC_ROOT/artefacts/httrack-scripts/conf-years/${i}/${CURR_YEAR_STR}/lineups-cli.sh
+  $PBP_SRC_ROOT/artefacts/httrack-scripts/conf-years/${i}/${CURR_YEAR_STR}/lineups-cli.sh
 
   java -cp "$PBP_SRC_ROOT/target/scala-2.12/cbb-explorer-assembly-0.1-deps.jar:$PBP_SRC_ROOT/target/scala-2.12/cbb-explorer_2.12-0.1.jar" \
     org.piggottfamily.cbb_explorer.BuildLineups \
@@ -32,4 +32,4 @@ grep "LineupErrorAnalysis" $PBP_OUT_DIR/bulk_import_logs_${CURR_TIME}.log
 
 # Import:
 echo "Importing new game data..."
-#$ELASTIC_FILEBEAT_BIN -c $ELASTIC_FILEBEAT_CONFIG_ROOT/filebeat_lineups.yaml --once
+$ELASTIC_FILEBEAT_BIN -c $ELASTIC_FILEBEAT_CONFIG_ROOT/filebeat_lineups.yaml --once

535d14af3725377639c10d7475792b5628f33585
Alex-At-Home <alex.piggottfamily.co.uk>
2021-01-22 10:38:45 -0500

    Add all patriot league teams to men's downloads

--- artefacts/httrack-scripts/conf-years/patriot/2020_21/lineups-cli.sh
+++ artefacts/httrack-scripts/conf-years/patriot/2020_21/lineups-cli.sh
@@ -8,8 +8,16 @@
 YEAR=2020
 CONF=patriot
 array=(
+   '153.0/15480::Colgate'
+   '342.0/15480::Lafayette'
    '725.0/15480::Army+West+Point'
+   '23.0/15480::American'
+   '369.0/15480::Loyola+Maryland'
    '726.0/15480::Navy'
+   '285.0/15480::Holy+Cross'
+   '83.0/15480::Bucknell'
+   '352.0/15480::Lehigh'
+   '68.0/15480::Boston+U.'
 )
 
 #TODO add TEAM filter

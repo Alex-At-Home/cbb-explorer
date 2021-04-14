75418a060fe1662d5ced69b7fa3dd090689789fb
Alex-At-Home <alex.piggottfamily.co.uk>
2021-04-13 21:11:16 -0400

    Improved postponed game detection handling

--- artefacts/scripts/daily_cbb_import.sh
+++ artefacts/scripts/daily_cbb_import.sh
@@ -3,7 +3,7 @@
 echo "Fix broken play-by-play files"
 
 for i in $(find $PBP_CRAWL_PATH -name "*.zip" | grep "/$CURR_YEAR/"); do
-   j=$(unzip -l $i | grep box_score | grep -E "\s+[0-9][0-9]\s+" | grep -o 'https:.*') && echo "$i /// $j" && zip -d $i "$j";
+   j=$(unzip -l $i | grep box_score | grep -E "\s+[0-9][0-9][0-9]?\s+" | grep -o 'https:.*') && echo "$i /// $j" && zip -d $i "$j";
 done
 
 ############

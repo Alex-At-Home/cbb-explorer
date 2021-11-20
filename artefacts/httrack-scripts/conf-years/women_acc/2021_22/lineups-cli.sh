c8f78614d3a0deb041a79eb7e223bd7433f9d991
Alex-At-Home <alex.piggott@elastic.co>
2021-11-20 10:51:13 -0500

    Add high major women to 21/22 ingest pipeline

---
+++ artefacts/httrack-scripts/conf-years/women_acc/2021_22/lineups-cli.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#(source .lineup.env first to set up these variables)
+YEAR=2021
+CONF=women_acc
+array=(
+   '490.0/15866::NC+State'
+   '367.0/15866::Louisville'
+   '457.0/15866::North+Carolina'
+   '193.0/15866::Duke'
+   '742.0/15866::Virginia+Tech'
+   '688.0/15866::Syracuse'
+   '147.0/15866::Clemson'
+   '513.0/15866::Notre+Dame'
+   '67.0/15866::Boston+College'
+   '749.0/15866::Wake+Forest'
+   '415.0/15866::Miami+%28FL%29'
+   '255.0/15866::Georgia+Tech'
+   '545.0/15866::Pittsburgh'
+   '234.0/15866::Florida+St.'
+   '746.0/15866::Virginia'
+)
+
+for index in "${array[@]}" ; do
+    FULLTEAMID="${index%%::*}"
+    TEAMID="${FULLTEAMID%%/*}"
+    SUBTEAMID="${TEAMID%%.*}"
+    YEARID="${FULLTEAMID##*/}"
+    TEAM_NAME="${index##*::}"
+    CONF_CRAWL_PATH=$PBP_CRAWL_PATH/$CONF/$YEAR/${TEAM_NAME}_${TEAMID}
+
+    if [ "$TEAMID_FILTER" != "" ]; then
+      if [ "$TEAMID_FILTER" != "$TEAMID" ]; then
+        echo "Skipping filtered $TEAM_NAME, [$TEAMID_FILTER] vs [$TEAMID]"
+        continue
+      fi
+    fi
+
+    echo "$FULLTEAMID ($TEAMID) - $TEAM_NAME"
+    #TODO: only do this if you want to remove and recalc everything, otherwise will find deltas
+    #rm -rf $CONF_CRAWL_PATH
+    mkdir -p $CONF_CRAWL_PATH
+    # Remove the main crawl file from the caches:
+    for file in old new; do
+      if [ -e $CONF_CRAWL_PATH/hts-cache/${file}.zip ]; then
+        for i in $(unzip -l $CONF_CRAWL_PATH/hts-cache/${file}.zip | grep -E "/teams?/" | awk '{ print $4 }'); do
+          zip -d $CONF_CRAWL_PATH/hts-cache/${file}.zip $i;
+        done
+      fi
+    done
+    httrack "$PBP_ROOT_URL/team/$FULLTEAMID" --continue --depth=3 --path $CONF_CRAWL_PATH --robots=0 "-*" "+$PBP_ROOT_URL/contests/*/box_score" "+$PBP_ROOT_URL/team/$SUBTEAMID/roster/$YEARID" "+$PBP_ROOT_URL/game/index/*" +"$PBP_ROOT_URL/game/box_score/*?period_no=1" +"$PBP_ROOT_URL/game/play_by_play/*"
+
+    #Check for any errors:
+    ERRS=$(grep -c 'Error:' $CONF_CRAWL_PATH/hts-log.txt)
+    if [ $ERRS -gt 0 ]; then
+      echo "************ ERRORS $CONF_CRAWL_PATH/hts-log.txt"
+      exit -1
+    fi
+done
---
+++ artefacts/httrack-scripts/conf-years/women_pactwelve/2021_22/lineups-cli.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#(source .lineup.env first to set up these variables)
+YEAR=2021
+CONF=women_pactwelve
+array=(
+   '674.0/15866::Stanford'
+   '110.0/15866::UCLA'
+   '528.0/15866::Oregon+St.'
+   '529.0/15866::Oregon'
+   '29.0/15866::Arizona'
+   '157.0/15866::Colorado'
+   '657.0/15866::Southern+California'
+   '754.0/15866::Washington+St.'
+   '732.0/15866::Utah'
+   '756.0/15866::Washington'
+   '28.0/15866::Arizona+St.'
+   '107.0/15866::California'
+)
+
+for index in "${array[@]}" ; do
+    FULLTEAMID="${index%%::*}"
+    TEAMID="${FULLTEAMID%%/*}"
+    SUBTEAMID="${TEAMID%%.*}"
+    YEARID="${FULLTEAMID##*/}"
+    TEAM_NAME="${index##*::}"
+    CONF_CRAWL_PATH=$PBP_CRAWL_PATH/$CONF/$YEAR/${TEAM_NAME}_${TEAMID}
+
+    if [ "$TEAMID_FILTER" != "" ]; then
+      if [ "$TEAMID_FILTER" != "$TEAMID" ]; then
+        echo "Skipping filtered $TEAM_NAME, [$TEAMID_FILTER] vs [$TEAMID]"
+        continue
+      fi
+    fi
+
+    echo "$FULLTEAMID ($TEAMID) - $TEAM_NAME"
+    #TODO: only do this if you want to remove and recalc everything, otherwise will find deltas
+    #rm -rf $CONF_CRAWL_PATH
+    mkdir -p $CONF_CRAWL_PATH
+    # Remove the main crawl file from the caches:
+    for file in old new; do
+      if [ -e $CONF_CRAWL_PATH/hts-cache/${file}.zip ]; then
+        for i in $(unzip -l $CONF_CRAWL_PATH/hts-cache/${file}.zip | grep -E "/teams?/" | awk '{ print $4 }'); do
+          zip -d $CONF_CRAWL_PATH/hts-cache/${file}.zip $i;
+        done
+      fi
+    done
+    httrack "$PBP_ROOT_URL/team/$FULLTEAMID" --continue --depth=3 --path $CONF_CRAWL_PATH --robots=0 "-*" "+$PBP_ROOT_URL/contests/*/box_score" "+$PBP_ROOT_URL/team/$SUBTEAMID/roster/$YEARID" "+$PBP_ROOT_URL/game/index/*" +"$PBP_ROOT_URL/game/box_score/*?period_no=1" +"$PBP_ROOT_URL/game/play_by_play/*"
+
+    #Check for any errors:
+    ERRS=$(grep -c 'Error:' $CONF_CRAWL_PATH/hts-log.txt)
+    if [ $ERRS -gt 0 ]; then
+      echo "************ ERRORS $CONF_CRAWL_PATH/hts-log.txt"
+      exit -1
+    fi
+done

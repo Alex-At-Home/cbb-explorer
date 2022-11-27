a1c35bc0f51a77ef969e2ef2225e90868c0a4a1d
Alex-At-Home <alex.piggott@elastic.co>
2022-11-26 19:57:05 -0500

    Add diag logs to women efficency upload script

--- artefacts/scripts/women_efficiency_import.sh
+++ artefacts/scripts/women_efficiency_import.sh
@@ -3,13 +3,16 @@
 TO_UPLOAD=$(ls -t $HOME/Downloads/export-[0-9]*.csv $HOME/Downloads/export.csv | head -n 1)
 
 if [[ "$TO_UPLOAD" != "" ]]; then
-   echo "women_efficiency_import: Uploading [$TO_UPLOAD]"
+   NUM_LINES=$(wc -l $TO_UPLOAD | awk '{ print $1 }')
+   echo "women_efficiency_import: Uploading [$NUM_LINES] teams from [$TO_UPLOAD]"
 
    curl "$EFF_WOMEN_TRIGGER_UPLOAD" --data-binary @$TO_UPLOAD
+   echo
 
    echo "women_efficiency_import: Deleting [$(ls -t $HOME/Downloads/export-[0-9]*.csv $HOME/Downloads/export.csv | wc -l)] old files"
    for i in $(ls -t $HOME/Downloads/export-[0-9]*.csv $HOME/Downloads/export.csv); do
-   rm $i
+      echo "women_efficiency_import: deleting [$i]"
+      rm $i
    done
 else
    echo "women_efficiency_import: Nothing to upload"

fc7d936ddedc55319c28bfb1bd49b849b3fac39f
Alex-At-Home <alex.piggott@elastic.co>
2023-02-23 16:35:05 -0500

    Fix on-ball defense parsing for latest Synergy format

---
+++ artefacts/scripts/build_onball_defense.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+java -cp \
+   "$PBP_SRC_ROOT/target/scala-2.12/cbb-explorer-assembly-0.1-deps.jar:$PBP_SRC_ROOT/target/scala-2.12/cbb-explorer_2.12-0.1.jar" \
+   org.piggottfamily.cbb_explorer.BuildOnBallDefense \
+   --in-team=$PBP_OUT_DIR/OnBallDefense/Teams_22_23.csv \
+   --in-player=$PBP_OUT_DIR/OnBallDefense/Indiv_22_23.csv \
+   --rosters=$HOOPEXP_SRC_DIR/public/rosters \
+   --out=$PBP_OUT_DIR/OnBallDefense/out \
+   --year=2022
--- src/main/scala/org/piggottfamily/cbb_explorer/BuildOnBallDefense.scala
+++ src/main/scala/org/piggottfamily/cbb_explorer/BuildOnBallDefense.scala
@@ -72,16 +72,23 @@ object BuildOnBallDefense {
 
       // Build team LUT
 
+      val team_lut_str = Source.fromURL(getClass.getResource("/onball_team_lookup.csv")).mkString         
+      type TeamLutEntry = (String, String)
+      val team_lut: Map[String, String] = team_lut_str.asCsvReader[TeamLutEntry](rfc).toList.flatMap(_.toOption).toMap
+
+
       // Read in team on-ball defense from file as CSV and enrich with the roster info
 
       val team_file = Path(in_team_file)
       val team_csv = read.lines(team_file).mkString("\n")
 
       case class TeamOnBallDefense(
-         ncaa_team: String, rank: String, team: String, gp: String, pct_time: String, poss: String, points: String,
-         ppp: String, fg_miss: String, fg_made: String, fga: String, fg_pct: String, efg_pct: String, 
-         to_pct: String, ft_pct: String, sf_pct: String, pct_score: String
+         rank: String, team: String, gp: String, pct_time: String,
+         poss: String, points: String, ppp: String, fg_miss: String, fg_made: String, fga: String, fg_pct: String, efg_pct: String, 
+         sq: String, sqPpp: String, sqM: String,
+         to_pct: String, ft_pct: String, pct_score: String, sf_pct: String
       )
+
       case class EnrichedTeamOnBallDefense(
          encoded_team: String,
          on_ball: TeamOnBallDefense,
@@ -93,20 +100,21 @@ object BuildOnBallDefense {
 
       val teams_map = team_csv.asCsvReader[TeamOnBallDefense](rfc.withHeader).toList.flatMap(_.toOption).flatMap { entry =>
 
-         val encoded_team_name = URLEncoder.encode(entry.ncaa_team, "UTF-8").replace(" ", "+")
+         val ncaa_team = team_lut.get(entry.team).getOrElse(entry.team)
+         val encoded_team_name = URLEncoder.encode(ncaa_team, "UTF-8").replace(" ", "+")
          Try {
             storage_controller.read_roster(Path(roster_dir) / s"Men_$year" / s"$encoded_team_name.json")
          }.recoverWith {
             case error =>
-               System.out.println(s"Failed to ingest [${entry.ncaa_team}][$encoded_team_name]: $error")
+               System.out.println(s"Failed to ingest [${ncaa_team}][$encoded_team_name]: $error")
                Failure(error)
          }.map(roster => (roster.values, LineupEvent( //Lots of dummy fields, only "players" field is populated
             DateTime.now(), Game.LocationType.Home, 0.0, 0.0, 0.0, LineupEvent.ScoreInfo.empty, 
-            TeamSeasonId(TeamId(entry.ncaa_team), Year(year)), TeamSeasonId(TeamId(entry.ncaa_team), Year(year)), LineupEvent.LineupId("none"),
+            TeamSeasonId(TeamId(ncaa_team), Year(year)), TeamSeasonId(TeamId(ncaa_team), Year(year)), LineupEvent.LineupId("none"),
             roster.values.toList.map(_.player_code_id), 
             Nil, Nil, Nil, LineupEventStats.empty, LineupEventStats.empty, None
          ))).map { case (roster, lineup) =>
-            (entry.ncaa_team, EnrichedTeamOnBallDefense(encoded_team = encoded_team_name, on_ball = entry, roster = roster.toList, box_lineup = lineup))
+            (ncaa_team, EnrichedTeamOnBallDefense(encoded_team = encoded_team_name, on_ball = entry, roster = roster.toList, box_lineup = lineup))
          }.toOption
          
       }.toMap
@@ -119,9 +127,10 @@ object BuildOnBallDefense {
       val player_csv = read.lines(player_file).mkString("\n")
 
       case class PlayerOnBallDefense(
-         ncaa_team: String, rank: String, player: String, team: String, gp: String, pct_time: String, poss: String, points: String,
+         rank: String, player: String, team: String, gp: String, pct_time: String, poss: String, points: String,
          ppp: String, fg_miss: String, fg_made: String, fga: String, fg_pct: String, efg_pct: String, 
-         to_pct: String, ft_pct: String, sf_pct: String, pct_score: String
+         sq: String, sqPpp: String, sqM: String,
+         to_pct: String, ft_pct: String, pct_score: String, sf_pct: String
       )
       case class EnrichedPlayerOnBallDefense(
          encoded_team: String,
@@ -131,7 +140,9 @@ object BuildOnBallDefense {
       )
 
       val players_grouped_by_team = 
-         player_csv.asCsvReader[PlayerOnBallDefense](rfc.withHeader).toList.flatMap(_.toOption).groupBy(_.ncaa_team)
+         player_csv.asCsvReader[PlayerOnBallDefense](rfc.withHeader).toList.flatMap(_.toOption).groupBy { entry =>
+            team_lut.get(entry.team).getOrElse(entry.team)
+         }
          
       val enriched_players_grouped_by_team = players_grouped_by_team.map { case (ncaa_team, entries) =>
 
@@ -142,8 +153,8 @@ object BuildOnBallDefense {
             case Some(enriched_team_entry) =>
 
                val tidy_ctx = LineupErrorAnalysisUtils.build_tidy_player_context(enriched_team_entry.box_lineup)
-               (ncaa_team, entries.flatMap { entry =>
-
+               (ncaa_team, entries.flatMap { entry => Try {
+                  
                   // This makes it easier to apply the fuzzy matcher
                   val name_frags = entry.player.split(" ")
                   val tidied_player_name = if (name_frags.size == 2) {
@@ -155,9 +166,12 @@ object BuildOnBallDefense {
                   val maybe_fixed_player_roster_entry = enriched_team_entry.roster.find(_.player_code_id.id.name == fixed_player)
 
                   maybe_fixed_player_roster_entry.map { roster_entry => 
-                     EnrichedPlayerOnBallDefense(enriched_team_entry.encoded_team, enriched_team_entry.on_ball, entry, s"#${roster_entry.number} ${roster_entry.player_code_id.code}")
+                     EnrichedPlayerOnBallDefense(
+                        enriched_team_entry.encoded_team, enriched_team_entry.on_ball, entry, 
+                        s"${roster_entry.player_code_id.code}"
+                     )
                   }
-               })
+               }.toOption.flatten.toList})
 
             case None => (ncaa_team, List())
          }
@@ -172,11 +186,11 @@ object BuildOnBallDefense {
          val td = entries.head.team //(must exist by construction)
          val encoded_team_name = entries.head.encoded_team
 
-         val team_row = s"$ncaa_team\t100%\t${td.poss}\t${td.points}\t${td.ppp}\trank\trating\t${td.fg_miss}\t${td.fg_made}\t${td.fga}\t${td.fg_pct}\t${td.efg_pct}\t${td.to_pct}\t${td.ft_pct}\t${td.sf_pct}\t${td.pct_score}"
+         val team_row = s"${td.rank},Team,Team,${td.gp},100%,${td.poss},${td.points},${td.ppp},${td.fg_miss},${td.fg_made},${td.fga},${td.fg_pct},${td.efg_pct},${td.to_pct},${td.ft_pct},${td.pct_score},${td.sf_pct}"
 
          val entry_rows = entries.map { entry =>
             val pd = entry.player
-            s"${entry.name_to_use}\t${pd.pct_time}\t${pd.poss}\t${pd.points}\t${pd.ppp}\t${pd.rank}\trating\t${pd.fg_miss}\t${pd.fg_made}\t${pd.fga}\t${pd.fg_pct}\t${pd.efg_pct}\t${pd.to_pct}\t${pd.ft_pct}\t${pd.sf_pct}\t${pd.pct_score}"
+            s"${pd.rank},${entry.name_to_use},${pd.team},${pd.gp},${pd.pct_time},${pd.poss},${pd.points},${pd.ppp},${pd.fg_miss},${pd.fg_made},${pd.fga},${pd.fg_pct},${pd.efg_pct},${pd.to_pct},${pd.ft_pct},${pd.pct_score},${pd.sf_pct}"
          }
 
          write.over(Path(out_path) / year.toString / s"${encoded_team_name}.txt", (List(team_row) ++ entry_rows).mkString("\n"))

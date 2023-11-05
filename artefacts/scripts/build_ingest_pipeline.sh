5d3856e80eda44c73711e4241c7c27d459696b8f
Alex-At-Home <alex.piggott@elastic.co>
2023-11-05 17:03:27 -0500

    Misc pre-season chores

--- artefacts/scripts/build_ingest_pipeline.sh
+++ artefacts/scripts/build_ingest_pipeline.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "NO LONGER USED - SEE build_another_year_httrack_script.sh"
+echo "To build AvailableTeams in cbb-on-off-analyzer, see AvailableTeams.byName docs"
+exit 1
+
 # mid majors:
 #CONFS="wcc,mountainwest,mvc,conferenceusa,mac,socon,sunbelt,bigsky,colonial,summit"
 
@@ -10,8 +14,17 @@
 #All: acc,american,atlanticten,bigeast,bigten,bigtwelve,pactwelve,sec,wcc,mountainwest,mvc,conferenceusa,mac,socon,sunbelt,bigsky,colonial,summit,americaeast,atlanticsun,bigsouth,bigwest,horizon,ivy,maac,meac,nec,ovc,patriot,southland,swac,wac
 #High major women: acc,american,bigeast,bigten,bigtwelve,pactwelve,sec
 #(misc_conf not currently supported)
+
 export REPLACE_EXIST=${REPLACE_EXIST:="no"}
-export YEAR=${YEAR:="2022_23"}
+export YEAR=${YEAR:="2023_24"}
+if [ "$GENDER" == "men" ]; then
+  export CONFS={CONFS:="acc,american,atlanticten,bigeast,bigten,bigtwelve,pactwelve,sec,wcc,mountainwest,mvc,conferenceusa,mac,socon,sunbelt,bigsky,colonial,summit,americaeast,atlanticsun,bigsouth,bigwest,horizon,ivy,maac,meac,nec,ovc,patriot,southland,swac,wac"}
+elif [ "$GENDER" == "women" ]; then
+  export CONFS={CONFS:="acc,american,bigeast,bigten,bigtwelve,pactwelve,sec"}
+else
+  echo "Need GENDER=men|women"
+  exit 1
+fi
 
 java -cp "$PBP_SRC_ROOT/target/scala-2.12/cbb-explorer-assembly-0.1-deps.jar:$PBP_SRC_ROOT/target/scala-2.12/cbb-explorer_2.12-0.1.jar" \
   org.piggottfamily.cbb_explorer.BuildIngestPipeline \

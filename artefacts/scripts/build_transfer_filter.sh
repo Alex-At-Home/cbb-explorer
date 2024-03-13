79a40e717cc0a20376302f5b310cd4f7b895f9f9
Alex-At-Home <alex.piggott@elastic.co>
2024-03-13 15:49:31 -0400

    Support running transfer and NBA declaration pipeline before NBA one exists

--- artefacts/scripts/build_transfer_filter.sh
+++ artefacts/scripts/build_transfer_filter.sh
@@ -4,6 +4,7 @@
 # (year is confusingly the first year of the most recent active season, eg 2022 == 2022/23 == 23 offseason!)
 # PROCESS_ONLY=yes to skip the download
 # TRANSFER=no to not copy to GS
+# IGNORE_NBA=yes to ignore NBA declarations (set this when running by hand before NBA pipeline is set up)
 # ie handy for debug: TRANSFER="no" PROCESS_ONLY="yes" CURR_YEAR=2022 bash /Users/alex/personal/github/cbb-explorer/artefacts/scripts/build_transfer_filter.sh
 #
 
@@ -85,13 +86,25 @@ NUM_NBA=$(grep -c 'www.sports-reference.com' $PBP_OUT_DIR/nba_declarations_$CURR
 echo "build_transfer_filter: [$(date)] Downloaded [$NUM_NBA] lines of NBA declarations"
 echo "build_transfer_filter: [$(date)] Downloaded [$NUM_TRANSFERS] transfers"
 
-if [[ $NUM_TRANSFERS -gt 0 ]] && [[ $NUM_NBA -gt 0 ]]; then
-   java -cp "$PBP_SRC_ROOT/target/scala-2.12/cbb-explorer-assembly-0.1-deps.jar:$PBP_SRC_ROOT/target/scala-2.12/cbb-explorer_2.12-0.1.jar" \
-      org.piggottfamily.cbb_explorer.BuildTransferLookup --in=$TXFER_FILE_TO_PARSE \
-      --in-nba=$PBP_OUT_DIR/nba_declarations_$CURR_YEAR_P1.html \
-      --rosters=$HOOPEXP_SRC_DIR/public/rosters --out=$PBP_OUT_DIR/transfers_$CURR_YEAR_P1.json --year=$CURR_YEAR
 
-   if [ "$TRANSFER" != "no" ]; then
-      gsutil cp $PBP_OUT_DIR/transfers_$CURR_YEAR_P1.json gs://$LEADERBOARD_BUCKET/ 
+if [[ $NUM_TRANSFERS -gt 0 ]]; then
+   if [[ $NUM_NBA -gt 0 ]]; then
+      java -cp "$PBP_SRC_ROOT/target/scala-2.12/cbb-explorer-assembly-0.1-deps.jar:$PBP_SRC_ROOT/target/scala-2.12/cbb-explorer_2.12-0.1.jar" \
+         org.piggottfamily.cbb_explorer.BuildTransferLookup --in=$TXFER_FILE_TO_PARSE \
+         --in-nba=$PBP_OUT_DIR/nba_declarations_$CURR_YEAR_P1.html \
+         --rosters=$HOOPEXP_SRC_DIR/public/rosters --out=$PBP_OUT_DIR/transfers_$CURR_YEAR_P1.json --year=$CURR_YEAR
+
+      if [ "$TRANSFER" != "no" ]; then
+         gsutil cp $PBP_OUT_DIR/transfers_$CURR_YEAR_P1.json gs://$LEADERBOARD_BUCKET/ 
+      fi
+   elif [ "$IGNORE_NBA" = "yes" ]; then
+      java -cp "$PBP_SRC_ROOT/target/scala-2.12/cbb-explorer-assembly-0.1-deps.jar:$PBP_SRC_ROOT/target/scala-2.12/cbb-explorer_2.12-0.1.jar" \
+         org.piggottfamily.cbb_explorer.BuildTransferLookup --in=$TXFER_FILE_TO_PARSE \
+         --rosters=$HOOPEXP_SRC_DIR/public/rosters --out=$PBP_OUT_DIR/transfers_$CURR_YEAR_P1.json --year=$CURR_YEAR
+
+      if [ "$TRANSFER" != "no" ]; then
+         gsutil cp $PBP_OUT_DIR/transfers_$CURR_YEAR_P1.json gs://$LEADERBOARD_BUCKET/ 
+      fi
    fi
+
 fi

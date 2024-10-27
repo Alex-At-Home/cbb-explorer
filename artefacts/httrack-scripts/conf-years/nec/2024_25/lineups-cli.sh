ab7d601eb205b5063e7527fc202b0ad20475baf6
Alex-At-Home <alex.piggott@elastic.co>
2024-10-27 16:51:37 -0400

    Port 23-24 and 24-25 import to new v1 format

--- artefacts/httrack-scripts/conf-years/nec/2024_25/lineups-cli.sh
+++ artefacts/httrack-scripts/conf-years/nec/2024_25/lineups-cli.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#(source .lineup.env first to set up these variables)
+source $PBP_SRC_ROOT/artefacts/httrack-scripts/v1_import.sh
+
 YEAR=2024
 CONF=nec
 array=(
@@ -13,39 +14,4 @@ array=(
    '136.0/16700::Chicago+St.'
 )
 
-for index in "${array[@]}" ; do
-    FULLTEAMID="${index%%::*}"
-    TEAMID="${FULLTEAMID%%/*}"
-    SUBTEAMID="${TEAMID%%.*}"
-    YEARID="${FULLTEAMID##*/}"
-    TEAM_NAME="${index##*::}"
-    CONF_CRAWL_PATH=$PBP_CRAWL_PATH/$CONF/$YEAR/${TEAM_NAME}_${TEAMID}
-
-    if [ "$TEAMID_FILTER" != "" ]; then
-      if [ "$TEAMID_FILTER" != "$TEAMID" ]; then
-        echo "Skipping filtered $TEAM_NAME, [$TEAMID_FILTER] vs [$TEAMID]"
-        continue
-      fi
-    fi
-
-    echo "$FULLTEAMID ($TEAMID) - $TEAM_NAME"
-    #TODO: only do this if you want to remove and recalc everything, otherwise will find deltas
-    #rm -rf $CONF_CRAWL_PATH
-    mkdir -p $CONF_CRAWL_PATH
-    # Remove the main crawl file from the caches:
-    for file in old new; do
-      if [ -e $CONF_CRAWL_PATH/hts-cache/${file}.zip ]; then
-        for i in $(unzip -l $CONF_CRAWL_PATH/hts-cache/${file}.zip | grep -E "/teams?/" | awk '{ print $4 }'); do
-          zip -d $CONF_CRAWL_PATH/hts-cache/${file}.zip $i;
-        done
-      fi
-    done
-    httrack "$PBP_ROOT_URL/team/$FULLTEAMID" --continue --depth=3 --path $CONF_CRAWL_PATH --robots=0 "-*" "+$PBP_ROOT_URL/contests/*/box_score" "+$PBP_ROOT_URL/team/$SUBTEAMID/roster/$YEARID" "+$PBP_ROOT_URL/game/index/*" +"$PBP_ROOT_URL/game/box_score/*?period_no=1" +"$PBP_ROOT_URL/game/play_by_play/*"
-
-    #Check for any errors:
-    ERRS=$(grep -c 'Error:' $CONF_CRAWL_PATH/hts-log.txt)
-    if [ $ERRS -gt 0 ]; then
-      echo "************ ERRORS $CONF_CRAWL_PATH/hts-log.txt"
-      exit -1
-    fi
-done
+import_data_v1 "$YEAR" "$CONF" "${array[@]}"

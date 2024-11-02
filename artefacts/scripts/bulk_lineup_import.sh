d7e9a0455ace63a0a316eb418b14ca51c2cb8528
Alex-At-Home <alex.piggott@elastic.co>
2024-11-02 17:15:51 -0400

    Got shot events uploading

--- artefacts/scripts/bulk_lineup_import.sh
+++ artefacts/scripts/bulk_lineup_import.sh
@@ -24,7 +24,7 @@ export CLOSE_EOF=${CLOSE_EOF:="false"}
 
 export CURR_TIME=${CURR_TIME:=$(date +"%s")}
 
-export CURR_YEAR_STR=${CURR_YEAR_STR:="2023_24"}
+export CURR_YEAR_STR=${CURR_YEAR_STR:="2024_25"}
 export CURR_YEAR=$(echo $CURR_YEAR_STR | cut -c1-4)
 
 export CONFS=${CONFS:="acc american atlanticten bigeast bigten bigtwelve pactwelve sec wcc mountainwest mvc conferenceusa mac socon sunbelt bigsky colonial summit americaeast atlanticsun bigsouth bigwest horizon ivy maac meac nec ovc patriot southland swac wac women_acc women_american women_bigeast women_bigten women_bigtwelve women_pactwelve women_sec women_misc_conf"}
@@ -79,7 +79,7 @@ for i in $CONFS; do
       org.piggottfamily.cbb_explorer.BuildLineups \
       --in=$PBP_CRAWL_PATH/${i}/${CURR_YEAR}/ \
       --out=$PBP_OUT_DIR \
-      --player-events $TEAM_FILTER \
+      --player-events --shot-events $TEAM_FILTER \
       --from=$CURR_TIME >> $PBP_OUT_DIR/bulk_import_logs_${CURR_TIME}.log
   else
     echo "Skipping parse"

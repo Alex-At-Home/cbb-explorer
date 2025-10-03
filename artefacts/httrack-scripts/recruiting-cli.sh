9054845c0846d36e724c08c88c7b3e3226da7d00
Alex-At-Home <alex.piggott@elastic.co>
2025-10-03 09:01:41 -0400

    Start to move crawling logic to one place

--- artefacts/httrack-scripts/recruiting-cli.sh
+++
@@ -1,4 +0,0 @@
-#REC_CRAWL_PATH=$1
-#REC_ROOT_URL=$2
-httrack "https://$REC_ROOT_URL/Season/2018-Basketball/CompositeTeamRankings" --depth 9 --path $REC_CRAWL_PATH/classes --robots=0 "-*" "+$REC_ROOT_URL/Season/*-Basketball/CompositeTeamRankings"  "-$REC_ROOT_URL/Season/*-Basketball/CompositeTeamRankings?*" "+$REC_ROOT_URL/Season/*-Basketball/CompositeTeamRankings*Page*"   -v
-httrack "https://$REC_ROOT_URL/Season/2018-Basketball/CompositeTeamRankings" --depth 3 --path $REC_CRAWL_PATH/teams --robots=0 "-*" "+$REC_ROOT_URL/Season/*-Basketball/CompositeTeamRankings"  "-$REC_ROOT_URL/Season/*-Basketball/CompositeTeamRankings?*" "+$REC_ROOT_URL/college/*/Season/*-Basketball/Commits" "-$REC_ROOT_URL/college/*/Season/*-Basketball/Commits?*"  -v
--- artefacts/httrack-scripts/reference-cli.sh
+++
@@ -1,3 +0,0 @@
-#REF_CRAWL_PATH=$1
-#REF_ROOT_URL=$2
-httrack "https://$REF_ROOT_URL/cbb/schools/#site_menu_link" --depth=3 --path $PBP_CRAWL_PATH --robots=0 "-*" "+$REF_ROOT_URL/cbb/schools/*/" "+$REF_ROOT_URL/cbb/schools/*/20*.html" -v
